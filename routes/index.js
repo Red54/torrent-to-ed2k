e2f0afc1acd553d01568afbbdca8b8297785bb40
谢致邦 (XIE Zhibang) <yeking@red54.com>
2019-06-15 20:50:22 +0800

    Add torrent status

--- routes/index.js
+++ routes/index.js
@@ -24,19 +24,23 @@ router.post('/', function(req, res, next) {
       torrent.deselect(0, torrent.pieces.length - 1, false);
       torrent.pause();
       torrent.files.forEach(function(file) {
-        if (file.ed2k) links.push('ed2k://|file|'+file.name+'|'+file.length+'|'+file.ed2k.toString('hex')+'|/')
+        if (file.ed2k) links.push('ed2k://|file|'+file.name+'|'+file.length+'|'+file.ed2k.toString('hex')+'|/');
       });
       res.render('index', { title: 'magnet2ed2k', magnet: infohash, links: links, msg: 'Found' });
     }
 
     var torrent = client.get(infohash);
     if (torrent)
-      onTorrent(torrent);
-    else
-      client.add(infohash, onTorrent);
-  } else {
+      if ('d13:announce-listle8:url-listlee' != torrent.torrentFile)
+        onTorrent(torrent);
+      else
+        res.render('index', { title: 'magnet2ed2k', magnet: infohash, msg: 'Torrent downloading, please wait' });
+    else {
+      client.add(infohash);
+      res.render('index', { title: 'magnet2ed2k', magnet: infohash, msg: 'Torrent added, please wait' });
+    }
+  } else
     res.render('index', { title: 'magnet2ed2k', msg: 'Invalid torrent identifier' });
-  }
 });
 
 module.exports = router;
